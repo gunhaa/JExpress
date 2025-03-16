47f6c4722a1b885eb4c454514e821db99308eb2d
gunha <wh8299@gmail.com>
2025-03-17 08:41:17 +0900

    feat : add constant

--- src/main/java/simple/Main.java
+++ src/main/java/simple/Main.java
@@ -16,6 +16,7 @@ public class Main {
         app.get(
                 "/members",
                                                 // 원래 해당 위치 DB와 연결되는 람다식을 넘기고 catch가 되면 error로 연결되어야함
+                                                // JPA Repo를 이용한 find
                 new ResponseSuccess(200, new Member("gunha", 10)),
                 new ResponseError(404, "Not Found")
         );
---
+++ src/main/java/simple/constant/Constant.java
@@ -0,0 +1,9 @@
+package simple.constant;
+
+public class Constant {
+
+    public static final String HTTP_METHOD_GET = "GET";
+    public static final String HTTP_METHOD_POST = "POST";
+    public static final String HTTP_METHOD_PUT = "PUT";
+    public static final String HTTP_METHOD_DELETE = "DELETE";
+}
