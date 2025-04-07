3cd39de2af6ff2a9b7bed9ac253598baf9cfc33d
gunhaa <wh8299@gmail.com>
2025-04-08 02:08:09 +0900

    refactoring : JExpress 기능 일부 리팩토링

--- src/main/resources/static/API/JExpress.js
+++ src/main/resources/static/API/JExpress.js
@@ -1,29 +1,31 @@
 
- table = document.querySelector("#MAIN-API-DOCS table");
 
-const createDocs = async (url) => {
-    const res = await fetch(url);
-    const data = await res.json();
-
-    data.sort((a, b) => a.url.length - b.url.length);
-
-    data.forEach(v => {
-        const fieldsStr = Object.entries(v.fields)
-            .map(([key, value]) => `${key}: ${value}`)
-            .join("<br>");
-
-        table.insertAdjacentHTML("beforeend", `
-            <tr>
-                <td>${v.customHttpMethod}</td>
-                <td>${v.url}</td>
-                <td>${v.returnType}</td>
-                <td>${fieldsStr}</td>
-            </tr>
-        `);
-    });
-};
 
 document.addEventListener("DOMContentLoaded", async () => {
+
+table = document.querySelector("#MAIN-API-DOCS table");
+
+    const createDocs = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        data.sort((a, b) => a.url.length - b.url.length);
+
+        data.forEach(v => {
+            const fieldsStr = Object.entries(v.fields)
+                .map(([key, value]) => `${key}: ${value}`)
+                .join("<br>");
+
+            table.insertAdjacentHTML("beforeend", `
+                <tr>
+                    <td>${v.customHttpMethod}</td>
+                    <td>${v.url}</td>
+                    <td>${v.returnType}</td>
+                    <td>${fieldsStr}</td>
+                </tr>
+            `);
+        });
+    };
 
     try {
         await createDocs("/api-docs/get/v1");
